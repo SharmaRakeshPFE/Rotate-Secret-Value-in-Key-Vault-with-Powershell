f8884f9191d7dbef9ee4299c9a1cde90b79b5066
SharmaRakeshPFE <63040045+SharmaRakeshPFE@users.noreply.github.com>
2022-01-11 16:31:14 +0530

    Add files via upload

---
+++ RotateNewSecret2KeyVault.ps1
@@ -0,0 +1,24 @@
+﻿##AUTHOR - RAKESH SHARMA##
+##MODULE 2#
+##DESC - Initiate once AD password\SQL login Password is Changed
+##Please test this script thouroughly on Dev\Test before hitting produciton##
+Install-Module Azure
+Install-Module Microsoft.PowerShell.SecretManagement
+Install-Module Microsoft.PowerShell.SecretStore
+Install-Module AzureRM.KeyVault
+Install-Module Az.KeyVault.
+
+
+Import-Module Microsoft.PowerShell.SecretManagement
+Import-Module Microsoft.PowerShell.SecretStore
+Import-Module AzureRM.KeyVault
+Import-Module Az.KeyVault
+
+Login-AzureRmAccount        
+$VaultName = "rskeyvaulttest"
+$SecretName = "SQLsa"
+$OldSecret = ConvertTo-SecureString -String 'P@ssword1234' -AsPlainText -Force
+$NewSecret = ConvertTo-SecureString -String 'P@ssword0987' -AsPlainText -Force
+$ContentType = 'txt'
+Set-AzureKeyVaultSecret -VaultName $VaultName -Name $SecretName -SecretValue $OldSecret -Disable
+Set-AzureKeyVaultSecret -VaultName $VaultName -Name $SecretName -SecretValue $NewSecret -ContentType $ContentType -Tags $Tags
